b3e01d50de80bf9c13e8ec563a70b72868cae338
A-Harry <44701569+A-Harry@users.noreply.github.com>
2021-07-02 16:28:08 -0400

    list active rooms to the chat page

--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -1,11 +1,14 @@
-export default function Rooms(props){
-    return(
+export default function Rooms(props) {
+    return (
         <div>
-            {props.rooms.map(room =>{
-                return(
-                    <button onClick= {props.onChangeRoom} value={room}>{room}</button>
-                )
-            })}
+            {props.rooms.map((room) => {
+                if (room.status === "Active") {
+                    return (
+                        <button key={room._id} onClick={props.onChangeRoom} value={room.name}>{room.name}</button>
+                    )
+                }
+            })
+            }
         </div>
     )
 }
